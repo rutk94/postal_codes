ce21b727b050a4faf97d38431d7e2ed26e9c2ad5
patryk <gryzabawy@o2.pl>
2025-04-10 18:26:42 +0200

    add get_capacities function

--- src/vehicles.py
+++ src/vehicles.py
@@ -1,4 +1,7 @@
+import math
 from pydantic import BaseModel
+
+from src.settings import CAPACITY_FACTOR
 
 
 VEHICLE_CODES: tuple[str, ...] = ('05-500', '41-300', '62-510')
@@ -7,6 +10,7 @@ VEHICLE_CODES: tuple[str, ...] = ('05-500', '41-300', '62-510')
 class Vehicle(BaseModel):
     """
     Represents vehicle attributes.
+
     Attributes:
         nr: int
             Number of vehicle
@@ -19,6 +23,7 @@ class Vehicle(BaseModel):
     nr: int
     code: str
     code_id: int
+    capacity: int = 0  # set by calling get_capacity()
 
 
 def get_vehicles(
@@ -26,17 +31,67 @@ def get_vehicles(
 ) -> list[Vehicle]:
     """
     Returns list of Vehicle objects.
+
     Args:
         codes: list[str]
             List of all postal codes
-        vehicle_codes:
+        vehicle_codes: tuple[str, ...]
             List of vehicles postal codes
     Returns:
         vehicles: list[Vehicle]
             List of Vehicle objects
     """
+
     vehicles: list[Vehicle] = [
         Vehicle(nr=i, code=vehicle_codes[i], code_id=codes.index(vehicle_codes[i]))
         for i in range(len(vehicle_codes))
     ]
+
     return vehicles
+
+
+def get_capacities(
+    amount_cases: int,
+    vehicles: list[Vehicle],
+    # max_demand: int
+    factor: float = CAPACITY_FACTOR,
+) -> list[int]:
+    """
+    Returns a list of capacity values for each vehicle,
+    calculated using the following operation:
+
+    amount_cases / len(vehicles) * factor
+
+    The result is rounded up and changed to "integer" type.
+    Each vehicle gets an equal capacity.
+    Vehicle objects gets the new attribute 'capacity'.
+
+    Args:
+        amount_cases: int
+            Amount of all cases
+        vehicles: list[Vehicle]
+            List of Vehicle objects
+        factor: float
+            Constant factor used to loosen the range of possibilities
+
+    Returns:
+        capacities: list[int]
+            List of capacity values for each vehicle
+            in order according to Vehicle objects list "vehicles"
+    """
+
+    # calculate capacity
+    value: int = int(math.ceil(amount_cases / len(vehicles) * factor))
+
+    # # check if value is smaller then max_demand
+    # if value >= max_demand:
+    #     value = max_demand
+
+    # actualize Vehicle attribute
+    for vehicle in vehicles:
+        vehicle.capacity = value
+
+    # generate capacities list
+    capacities: list[int] = [vehicle.capacity for vehicle in vehicles]
+
+    return capacities
--- tests/test_vehicles.py
+++ tests/test_vehicles.py
@@ -1,8 +1,11 @@
-from src.vehicles import get_vehicles, Vehicle
+from pytest import fixture
+
+from src.vehicles import Vehicle, get_vehicles, get_capacities
 
 
-def test_get_vehicles() -> None:
-    codes: list[str] = [
+@fixture
+def setup_codes() -> list[str]:
+    return [
         '02-777',  # 1 (0)
         '02-232',  # 2 (1)
         '05-500',  # 3 (2)
@@ -11,17 +14,45 @@ def test_get_vehicles() -> None:
         '57-432',  # 6 (5)
         '16-070',  # 7 (6)
     ]
-    vehicle_codes: tuple[str, ...] = (
+
+
+@fixture
+def setup_vehicle_codes() -> tuple[str, ...]:
+    return (
         '05-500',  # 3 (2)
         '41-300',  # 4 (3)
         '62-510',  # 5 (4)
     )
-    vehicles: list[Vehicle] = get_vehicles(codes=codes, vehicle_codes=vehicle_codes)
 
+
+def test_get_vehicles(setup_codes: list[str], setup_vehicle_codes: tuple[str, ...]) -> None:
+    vehicles: list[Vehicle] = get_vehicles(
+        codes=setup_codes, vehicle_codes=setup_vehicle_codes
+    )
     vehicle_nrs: list[int] = [vehicle.nr for vehicle in vehicles]
+
     assert sorted(vehicle_nrs) == sorted(set(vehicle_nrs))
-    assert all(vehicle.code in codes for vehicle in vehicles)
+    assert all(vehicle.code in setup_codes for vehicle in vehicles)
     assert all(
-        vehicles[i].code_id == codes.index(vehicles[i].code)
+        vehicles[i].code_id == setup_codes.index(vehicles[i].code)
         for i in range(len(vehicles))
     )
+
+
+def test_get_capacities(
+    setup_codes: list[str], setup_vehicle_codes: tuple[str, ...]
+) -> None:
+    vehicles: list[Vehicle] = get_vehicles(
+        codes=setup_codes, vehicle_codes=setup_vehicle_codes
+    )
+    capacities: list[int] = get_capacities(
+        amount_cases=100, vehicles=vehicles, factor=1.1
+    )
+
+    assert len(capacities) != 0
+    assert len(capacities) == len(vehicles)
+    assert all(isinstance(value, int) for value in capacities)
+    assert all(capacities[i] == vehicles[i].capacity for i in range(len(capacities)))
+
+    # check if all capacities are equal
+    assert len(set(capacities)) == 1
