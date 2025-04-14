64923b9bcb10ef0820a91100d71bbf71aa1b5fb8
patryk <gryzabawy@o2.pl>
2025-04-14 20:26:07 +0200

    adjust solver to last changes

--- src/solver_ortools.py
+++ src/solver_ortools.py
@@ -29,10 +29,9 @@ class Solver:
         amount: int,
         starts: list,
         ends: list,
-        # nodes: list,
+        nodes: list,
         capacities: list[int],
-        # TODO: add global variables
-        # max_single_distance_enabled: bool = True,  # MAX_SINGLE_DISTANCE_ENABLED
+        max_single_distance_enabled: bool = True,  # MAX_SINGLE_DISTANCE_ENABLED
         max_total_distance: Optional[int] = MAX_TOTAL_DISTANCE,
         time_limit: Optional[int] = TIME_LIMIT,
         solution_limit: Optional[int] = SOLUTION_LIMIT,
@@ -41,7 +40,7 @@ class Solver:
         self.amount: int = amount
         self.starts: list = starts
         self.ends: list = ends
-        # self.nodes: list = nodes
+        self.nodes: list = nodes
         self.capacities: list[int] = capacities
 
         # self.max_single_distance_enabled: bool = max_single_distance_enabled
@@ -50,20 +49,23 @@ class Solver:
         self.solution_limit: Optional[int] = solution_limit
 
         self.manager = pywrapcp.RoutingIndexManager(
-            # len(self.nodes),
-            len(self.dist_matrix),
+            len(self.nodes),
+            # len(self.dist_matrix),
             self.amount,
             self.starts,
             self.ends,
         )
         self.routing = pywrapcp.RoutingModel(self.manager)
 
-        # if max_single_distance_enabled:
-        #     for node in self.nodes:
-        #         self.routing.SetAllowedVehiclesForIndex(
-        #             vehicles=node.possible_employees_nrs,
-        #             index=self.manager.NodeToIndex(node.node_id)
-        #         )
+        if max_single_distance_enabled:
+            # vehicles_ids: list[int] = [node.matrix_id for node in self.nodes if node.is_vehicle ]
+            for node in self.nodes:
+                self.routing.SetAllowedVehiclesForIndex(
+                    # vehicles=node.possible_vehicles,
+                    # vehicles=[vehicle.matrix_id for vehicle in node.possible_vehicles],
+                    vehicles=[vehicle.vehicle_id for vehicle in node.possible_vehicles],
+                    index=self.manager.NodeToIndex(node.node_id)
+                )
 
         # placeholder
         self.solution = None
@@ -73,10 +75,10 @@ class Solver:
         # TODO: finish function
         from_node = self.manager.IndexToNode(from_index)
         to_node = self.manager.IndexToNode(to_index)
-        # index_from = self.nodes[from_node].matrix_id
-        # index_to = self.nodes[to_node].matrix_id
-        # return self.dist_matrix[index_from][index_to]
-        return self.dist_matrix[from_node][to_node]
+        index_from = self.nodes[from_node].matrix_id
+        index_to = self.nodes[to_node].matrix_id
+        return self.dist_matrix[index_from][index_to]
+        # return self.dist_matrix[from_node][to_node]
 
     def _distance_callback_symmetrical(self, from_index, to_index):
         """Returns minimal distance between two points"""
@@ -88,9 +90,9 @@ class Solver:
     def _demand_callback(self, from_index):
         """Returns demand at given point"""
         # TODO: finish function
-        # from_node = self.manager.IndexToNode(from_index)
-        # return self.nodes[from_node].demand_weight
-        return 1
+        from_node = self.manager.IndexToNode(from_index)
+        return self.nodes[from_node].demand
+        # return 1
 
     def solve(self):
         # add distance dimension
@@ -136,7 +138,7 @@ class Solver:
 
         # return solution or raise error
         if self.solution:
-            return self.solution
+            return
         else:
             # TODO: add dictionary of statuses from: developers.google.com/optimization/routing/routing_options
             print(self.routing.status())
